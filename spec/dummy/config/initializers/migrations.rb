ce5c7de4383f44a93d92c256fb03821ee94667ec
Kyle Fox <kylefox@gmail.com>
2023-07-13 11:34:12 -0600

    Update migrations.rb

--- spec/dummy/config/initializers/migrations.rb
+++ spec/dummy/config/initializers/migrations.rb
@@ -1,3 +1,5 @@
+# This allows us to create migrations in the engine and have them automatically run in the dummy app
+# (when executed from either engine root or dummy root) without having to run `rails restable:install:migrations` each time.
 ActiveRecord::Migrator.migrations_paths = [
   File.expand_path("../../../../db/migrate", __dir__),
   File.expand_path("../../db/migrate", __dir__),
